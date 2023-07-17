c7b647ec89bf926687f70728ad067205d569d2f3
vikrant <vikrantkumar92498@gmail.com>
2023-07-17 17:23:46 +0530

    auth controllers -login and register

---
+++ controllers/authControllers.js
@@ -0,0 +1,37 @@
+const User = require("../Models/userModel");
+const bcrypt = require("bcrypt");
+
+exports.userRegister = async (req, res) => {
+    const {username , email,password } = req.body;
+    try{ 
+        const salt = await bcrypt.genSalt(10);
+        const hashedPass = await bcrypt.hash(password, salt);
+        const newUser = new User({
+        username: username,
+        email: email,
+        password: hashedPass,
+        });
+        const user = await newUser.save();
+        res.status(200).json(user);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+};
+ 
+
+exports.userLogin = async(req,res)=>{
+    const {email,password} = req.body;
+    try {
+        const user = await User.findOne({ email: email });
+        if (!user) {
+          return res.status(400).json("Wrong credentials!");
+        }
+        const validated = await bcrypt.compare(password, user.password);
+        if (!validated) {
+          return res.status(400).json("Wrong credentials!");
+        }
+        res.status(200).json(user);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+};
